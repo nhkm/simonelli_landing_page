57c6b44efc0cd9bfddbcc47ebdc4a15fda6092ad
Erkin <llyonnj@gmail.com>
2019-08-22 21:10:46 +0500

    first commit

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="products">
+        <div class="container">
+    	    <div class="col-md-12">
+			    <div id="cbp-vm" class="cbp-vm-switcher cbp-vm-view-grid">
+					<ul>
+					    <li>
+							<a class="cbp-vm-image" href="single.html">
+							    <div class="view view-first">
+					   		        <div class="inner_content clearfix">
+								        <div class="product_image">
+									        <div class="mask1"><img src="images/simonelli/p1.jpg" alt="image" class="img-responsive zoom-img"></div>
+									        <div class="mask">
+			                       		        <div class="info">Quick View</div>
+					                        </div>
+					                        <div class="product_container">
+									            <h4>Appia II</h4>
+									            <p>КОФЕМАШИНЫ</p>
+									            <div class="price">16 000 TMT</div>
+									        </div>		
+								        </div>
+			                        </div>
+		                        </div>
+		                    </a>
+						</li>
+
+						<li>
+							<a class="cbp-vm-image" href="single.html">
+							    <div class="view view-first">
+					   		        <div class="inner_content clearfix">
+								        <div class="product_image">
+									        <div class="mask1"><img src="images/simonelli/p1.jpg" alt="image" class="img-responsive zoom-img"></div>
+									        <div class="mask">
+			                       		        <div class="info">Quick View</div>
+					                        </div>
+					                        <div class="product_container">
+									            <h4>Appia II</h4>
+									            <p>КОФЕМАШИНЫ</p>
+									            <div class="price">12 000 TMT</div>
+									        </div>		
+								        </div>
+			                        </div>
+		                        </div>
+		                    </a>
+						</li>
+
+						<li>
+							<a class="cbp-vm-image" href="single.html">
+							    <div class="view view-first">
+					   		        <div class="inner_content clearfix">
+								        <div class="product_image">
+									        <div class="mask1"><img src="images/simonelli/p1.jpg" alt="image" class="img-responsive zoom-img"></div>
+									        <div class="mask">
+			                       		        <div class="info">Quick View</div>
+					                        </div>
+					                        <div class="product_container">
+									            <h4>Appia II</h4>
+									            <p>КОФЕМАШИНЫ</p>
+									            <div class="price">3 000 TMT</div>
+									        </div>		
+								        </div>
+			                        </div>
+		                        </div>
+		                    </a>
+						</li>
+
+
+					</ul>
+                </div>
+    	    </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,305 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Nuova Simonelli</title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--webfont-->
+<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="js/hover_pack.js"></script>
+<!-- start menu -->
+<link href="css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/megamenu.js"></script>
+<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+<script type="text/javascript">
+					jQuery(document).ready(function($) {
+						$(".scroll").click(function(event){		
+							event.preventDefault();
+							$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+						});
+					});
+</script>
+</head>
+<body>
+	<div class="banner">
+   	  	<div class="container">
+   			<div class="logo">
+				<a href="{{ '/' }}"><img src="images/simonelli/nuovalogo.svg" alt="Nuova Simonelli"/></a>
+			</div>
+	   		<div class="menu">
+				<ul class="megamenu skyblue">
+					<li><a class="color2" href="{{ '/products' }}">КОФЕМАШИНЫ</a>
+						<div class="megapanel">
+							<div class="row">
+								<div class="col1">
+									<div class="h_nav">
+										<h4>КОФЕМАШИНЫ</h4>
+										<ul>
+											<li><a href="{{ '/products' }}">Appia II</a></li>
+											<li><a href="{{ '/products' }}">Aurelia II</a></li>
+											<li><a href="{{ '/products' }}">Microbar II</a></li>
+											<li><a href="{{ '/products' }}">Musica</a></li>
+											<li><a href="{{ '/products' }}">Oscar II</a></li>
+											<li><a href="{{ '/products' }}">Prontobar</a></li>
+											<li><a href="{{ '/products' }}">Talento</a></li>
+										</ul>	
+									</div>							
+								</div>
+								<div class="col1">
+									<div class="h_nav">
+										<h4>КОФЕМОЛКИ</h4>
+										<ul>
+											<li><a href="{{ '/products' }}">Grinta</a></li>
+											<li><a href="{{ '/products' }}">MCF</a></li>
+											<li><a href="{{ '/products' }}">MDE</a></li>
+											<li><a href="{{ '/products' }}">MDJ</a></li>
+											<li><a href="{{ '/products' }}">MDK</a></li>
+											<li><a href="{{ '/products' }}">MDX</a></li>
+											<li><a href="{{ '/products' }}">MDSX</a></li>
+											<li><a href="{{ '/products' }}">Mythos</a></li>
+											<li><a href="{{ '/products' }}">F64</a></li>
+										</ul>	
+									</div>							
+								</div>
+								<div class="col2">
+									<div class="h_nav">
+										<h4>АКСЕССУАРЫ</h4>
+										<ul>
+											<li class>
+												<div class="p_left">
+												<img src="images/simonelli/p3.jpg" class="img-responsive" style="height: 80px; width: 60px;"/>
+												</div>
+												<div class="p_right">
+													<h4><a href="{{ '/products' }}">Аксессуар 1</a></h4>
+													<span class="item-cat"><small><a href="#">Аксессуар</a></small></span>
+													<span class="price">39 TMT</span>
+												</div>
+												<div class="clearfix"> </div>
+											</li>
+											<li>
+												<div class="p_left">
+												<img src="images/simonelli/p3.jpg" class="img-responsive" style="height: 80px; width: 60px;"/>
+												</div>
+												<div class="p_right">
+													<h4><a href="{{ '/products' }}">Аксессуар 2</a></h4>
+													<span class="item-cat"><small><a href="#">Аксессуар</a></small></span>
+													<span class="price">39 TMT</span>
+												</div>
+												<div class="clearfix"> </div>
+											</li>
+											<li>
+												<div class="p_left">
+												<img src="images/simonelli/p3.jpg" class="img-responsive" style="height: 80px; width: 60px;"/>
+												</div>
+												<div class="p_right">
+													<h4><a href="{{ '/products' }}">Аксессуар 3</a></h4>
+													<span class="item-cat"><small><a href="#">Аксессуар</a></small></span>
+													<span class="price">39 TMT</span>
+												</div>
+												<div class="clearfix"> </div>
+											</li>
+										</ul>	
+									</div>												
+								</div>
+							</div>
+						</div>
+					</li>
+					<li><a class="color4" href="{{ '/products' }}">КОФЕМОЛКИ</a>
+						<div class="megapanel">
+							<div class="row">
+								<div class="col1">
+									<div class="h_nav">
+										<h4>КОФЕМАШИНЫ</h4>
+										<ul>
+											<li><a href="{{ '/products' }}">Appia II</a></li>
+											<li><a href="{{ '/products' }}">Aurelia II</a></li>
+											<li><a href="{{ '/products' }}">Microbar II</a></li>
+											<li><a href="{{ '/products' }}">Musica</a></li>
+											<li><a href="{{ '/products' }}">Oscar II</a></li>
+											<li><a href="{{ '/products' }}">Prontobar</a></li>
+											<li><a href="{{ '/products' }}">Talento</a></li>
+										</ul>	
+									</div>							
+								</div>
+								<div class="col1">
+									<div class="h_nav">
+										<h4>КОФЕМОЛКИ</h4>
+										<ul>
+											<li><a href="{{ '/products' }}">Grinta</a></li>
+											<li><a href="{{ '/products' }}">MCF</a></li>
+											<li><a href="{{ '/products' }}">MDE</a></li>
+											<li><a href="{{ '/products' }}">MDJ</a></li>
+											<li><a href="{{ '/products' }}">MDK</a></li>
+											<li><a href="{{ '/products' }}">MDX</a></li>
+											<li><a href="{{ '/products' }}">MDSX</a></li>
+											<li><a href="{{ '/products' }}">Mythos</a></li>
+											<li><a href="{{ '/products' }}">F64</a></li>
+										</ul>	
+									</div>							
+								</div>
+								<div class="col2">
+									<div class="h_nav">
+										<h4>АКСЕССУАРЫ</h4>
+										<ul>
+											<li class>
+												<div class="p_left">
+												<img src="images/simonelli/p3.jpg" class="img-responsive" style="height: 80px; width: 60px;"/>
+												</div>
+												<div class="p_right">
+													<h4><a href="{{ '/products' }}">Аксессуар 1</a></h4>
+													<span class="item-cat"><small><a href="#">Аксессуар</a></small></span>
+													<span class="price">39 TMT</span>
+												</div>
+												<div class="clearfix"> </div>
+											</li>
+											<li>
+												<div class="p_left">
+												<img src="images/simonelli/p3.jpg" class="img-responsive" style="height: 80px; width: 60px;"/>
+												</div>
+												<div class="p_right">
+													<h4><a href="{{ '/products' }}">Аксессуар 2</a></h4>
+													<span class="item-cat"><small><a href="#">Аксессуар</a></small></span>
+													<span class="price">39 TMT</span>
+												</div>
+												<div class="clearfix"> </div>
+											</li>
+											<li>
+												<div class="p_left">
+												<img src="images/simonelli/p3.jpg" class="img-responsive" style="height: 80px; width: 60px;"/>
+												</div>
+												<div class="p_right">
+													<h4><a href="{{ '/products' }}">Аксессуар 3</a></h4>
+													<span class="item-cat"><small><a href="#">Аксессуар</a></small></span>
+													<span class="price">39 TMT</span>
+												</div>
+												<div class="clearfix"> </div>
+											</li>
+										</ul>	
+									</div>												
+								</div>
+							</div>
+						</div>
+					</li>				
+					<li><a class="color10" href="{{ '/products' }}">АКСЕССУАРЫ</a>
+						<div class="megapanel">
+							<div class="row">
+								<div class="col1">
+									<div class="h_nav">
+										<h4>КОФЕМАШИНЫ</h4>
+										<ul>
+											<li><a href="{{ '/products' }}">Appia II</a></li>
+											<li><a href="{{ '/products' }}">Aurelia II</a></li>
+											<li><a href="{{ '/products' }}">Microbar II</a></li>
+											<li><a href="{{ '/products' }}">Musica</a></li>
+											<li><a href="{{ '/products' }}">Oscar II</a></li>
+											<li><a href="{{ '/products' }}">Prontobar</a></li>
+											<li><a href="{{ '/products' }}">Talento</a></li>
+										</ul>	
+									</div>							
+								</div>
+								<div class="col1">
+									<div class="h_nav">
+										<h4>КОФЕМОЛКИ</h4>
+										<ul>
+											<li><a href="{{ '/products' }}">Grinta</a></li>
+											<li><a href="{{ '/products' }}">MCF</a></li>
+											<li><a href="{{ '/products' }}">MDE</a></li>
+											<li><a href="{{ '/products' }}">MDJ</a></li>
+											<li><a href="{{ '/products' }}">MDK</a></li>
+											<li><a href="{{ '/products' }}">MDX</a></li>
+											<li><a href="{{ '/products' }}">MDSX</a></li>
+											<li><a href="{{ '/products' }}">Mythos</a></li>
+											<li><a href="{{ '/products' }}">F64</a></li>
+										</ul>	
+									</div>							
+								</div>
+								<div class="col2">
+									<div class="h_nav">
+										<h4>АКСЕССУАРЫ</h4>
+										<ul>
+											<li class>
+												<div class="p_left">
+												<img src="images/simonelli/p3.jpg" class="img-responsive" style="height: 80px; width: 60px;"/>
+												</div>
+												<div class="p_right">
+													<h4><a href="{{ '/products' }}">Аксессуар 1</a></h4>
+													<span class="item-cat"><small><a href="#">Аксессуар</a></small></span>
+													<span class="price">39 TMT</span>
+												</div>
+												<div class="clearfix"> </div>
+											</li>
+											<li>
+												<div class="p_left">
+												<img src="images/simonelli/p3.jpg" class="img-responsive" style="height: 80px; width: 60px;"/>
+												</div>
+												<div class="p_right">
+													<h4><a href="{{ '/products' }}">Аксессуар 2</a></h4>
+													<span class="item-cat"><small><a href="#">Аксессуар</a></small></span>
+													<span class="price">39 TMT</span>
+												</div>
+												<div class="clearfix"> </div>
+											</li>
+											<li>
+												<div class="p_left">
+												<img src="images/simonelli/p3.jpg" class="img-responsive" style="height: 80px; width: 60px;"/>
+												</div>
+												<div class="p_right">
+													<h4><a href="{{ '/products' }}">Аксессуар 3</a></h4>
+													<span class="item-cat"><small><a href="#">Аксессуар</a></small></span>
+													<span class="price">39 TMT</span>
+												</div>
+												<div class="clearfix"> </div>
+											</li>
+										</ul>	
+									</div>												
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="active grid"><a class="color3" href="{{ '/contacts'}}">КОНТАКТЫ</a></li>
+					<div class="clearfix"> </div>
+				</ul>
+			</div>
+
+			<div class="clearfix"> </div>
+
+			<div class="main">
+				<div class="row">
+					<div class="col-12 main_content" style="font-size: 26px;">
+						<p><strong>NUOVA SIMONELLI - всемирно известный итальянский производитель профессиональных кофемашин и кофемолок. Компания выпускает полуавтоматические, автоматические и суперавтоматические кофемашины, позволяющие приготовить как классический кофе эспрессоб так и различные кофейные напитки с добавлением молока и других ингредиентов.</strong></p>
+					</div>
+				</div>
+			</div>
+
+			<div class="footer">
+				<div class="container">
+					<div class="cssmenu">
+						<ul>
+							<li><a href="{{ '/terms' }}">Terms</a></li>
+							<li><a href="{{ '/products' }}">Shop</a></li>
+							<li><a href="{{ '/' }}">About</a></li>
+							<li><a href="{{ '/contacts' }}">Contact</a></li>
+						</ul>
+					</div>
+					<ul class="social">
+						<li><a href="https://instagram.com/simonelli"><img src="../images/simonelli/instagram.png" alt="instagram"></a></li>
+						<li><a href="https://twitter.com/simonelli"><img src="../images/simonelli/twitter.png" alt="twitter"></a></li>
+					</ul>
+					<div class="clearfix"></div>
+					<div class="copy">
+						<p>&copy; Copyright 2019
+							<a href="http://simonelli.com.tm/" target="_blank">Nuova Simonelli</a>
+						</p>
+					</div>
+				</div>
+			</div>
+    	</div>
+	</div>
+</body>
+</html>		
